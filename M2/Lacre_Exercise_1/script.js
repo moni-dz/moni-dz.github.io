d355753d5e999e67f0da74c2db86cd3c5b3a7c70
moni-dz <lythe1107@gmail.com>
2026-02-10 06:29:43 +0800

    merge

--- M2/Lacre_Exercise_1/script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('changeTextBtn');
-const header = document.getElementById('header');
-
-button.addEventListener('click', () => {
-    header.textContent = 'Text Changed!';
-});
